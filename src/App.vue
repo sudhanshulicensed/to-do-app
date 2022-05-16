1024bb3747fd48923ff040009ddf29ace7918d51
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2022-05-16 18:10:55 +0530

    Added Functionality to Add/Delete Task

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,126 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="header">
+        <div class="title">To-Do Application</div>
+        <div class="add-todo">
+          <img @click="show = !show" src="./assets/Groupadd-btn.svg" alt="Adding Task to ToDo">
+          <AddToDo v-show="show" @myTask="addTask"/>
+        </div>
+      </div>
+      <div class="main">
+        <li v-for="(task, index) in myData" :key="index">
+          <div class="check">
+            <input type="checkbox">
+          </div>
+          <div class="content">
+            <div class="obj">
+              <p>{{task.conObj}}</p>
+              </div>
+            <div class="date-month">
+              <p>{{task.myDate}}/{{task.myMonth +1}}</p>
+            </div>
+            <div class="time">
+              <p>Time: {{task.myHours}}:{{task.myMinutes}}</p>
+            </div>
+          </div>
+          <div @click="editTask" class="edit"><p>Edit</p></div>
+          <div @click="deleteTask(index)" class="delete"><p>Delete</p></div>
+        </li>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AddToDo from "./components/AddToDo.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddToDo,
+  },
+  data() {
+    return {
+      content: "",
+      isEdit: false,
+      editIndex: null,
+      time: "",
+      date: new Date(),
+      show: false,
+      myData: [],
+
+    }
+  },
+  methods: {
+    addTask(obj) {
+      this.myData.push({conObj: obj.content, myDate: this.date.getDate(), myMonth: this.date.getMonth(), myHours: this.date.getHours(), myMinutes: this.date.getMinutes()});
+      this.show = !this.show;
+      console.log(this.date.getDate(), this.date.getHours(), this.date.getMinutes());
+    },
+
+    editTask(index) {
+      this.show = !this.show;
+      this.editIndex = index;
+      this.isEdit = true;
+      this.content = this.myData[index].conObj;
+    },
+
+    deleteTask(index){
+      this.myData.splice(index, 1);
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
 }
+
+
+.container{
+  display: flex;
+  flex-direction: column;
+}
+
+.header{
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.add-todo img{
+  display: flex;
+  align-items: center;
+}
+
+.main{
+  display: flex;
+  flex-direction: column;
+}
+
+li{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
+.content{
+  display: flex;
+  column-gap: 5px;
+  row-gap: 5px;
+  grid-column-start: 2;
+  grid-column-end: 3;
+}
+
+.check{
+  display: flex;
+  align-items: center;
+}
+
 </style>
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="content">
+        <form v-on:submit.prevent>
+            <label for="context">What are you up for?</label>
+            <input v-model="subject.content" id="context" type="text">
+            <button @click="addMyTask">Add Task</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddToDo",
+    data(){
+        return {
+            subject: {
+                content: "",
+            },
+        }
+    },
+    methods: {
+        addMyTask() {
+            this.$emit("myTask", this.subject);
+            this.subject.content = "";
+        }
+    }
+}
+</script>
+
+<style>
+    .content{
+        /* position: absolute; */
+        /* width: 300px; */
+        /* height: auto; */
+        /* margin: 10px; */
+    }
+</style>
